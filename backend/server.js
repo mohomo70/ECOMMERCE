86f209e8080e9cbb4c83bf5100d1493575722e78
mohammad hossein mohammadi <mohammadi_mh@yaftar.ir>
2022-10-30 14:05:51 +0330

    deploy

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,9 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
+// app.get('/', (req, res) => {
+//   res.send('API is running....')
+// })
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -33,13 +33,25 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
-  PORT , '172.30.106.118',
+  PORT ,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
